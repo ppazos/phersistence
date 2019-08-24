b21da83fe1db0ebc37bb77e72822de5ad821a86f
Pablo Pazos Gutierrez <pablo.pazos@cabolabs.com>
2019-08-23 22:52:07 -0300

    added convention over class and field names part of #49

--- basic/BasicString.php
+++ basic/BasicString.php
@@ -37,6 +37,55 @@ class BasicString {
     }
     return $str;
   }
+
+  static function generate_uuid()
+  {
+  	return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+  		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+  		mt_rand( 0, 0xffff ),
+  		mt_rand( 0, 0x0fff ) | 0x4000,
+  		mt_rand( 0, 0x3fff ) | 0x8000,
+  		mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+  	);
+  }
+
+  // Random string of desired length from full alphabet
+  function random($length = 10)
+  {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++)
+    {
+      $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+  }
+
+  // Random string of desired length, max 32, composed of HEX digits only
+  static function random_hex($length = 10)
+  {
+    if ($length > 32) $length = 32;
+    return substr(str_shuffle(md5(microtime())), 0, $length);
+  }
+
+  // Transforms ABc into a_bc
+  static function camel_to_snake($input)
+  {
+    preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+    $ret = $matches[0];
+    foreach ($ret as &$match) {
+      $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+    }
+    return implode('_', $ret);
+  }
+
+  static function snake_to_camel($input)
+  {
+    // TBD
+  }
 }
+
+
 
 ?>
